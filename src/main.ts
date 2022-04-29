f483a543fa5af8333a6f049c36b93f77921d868b
Austin <augustineumeagudosi@gmail.com>
2022-04-29 13:10:42 +0100

    finished postgres integration

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Ecommerce API')
+    .setDescription('Ecommerce API Documentation')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
